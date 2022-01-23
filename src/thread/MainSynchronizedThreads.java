d94c5d513242f48a0165e4afd7c9d25cfe95429e
Vacant-name <Kerambit50>
2022-01-23 18:39:34 +0300

    Synchronized method

---
+++ src/thread/MainSynchronizedThreads.java
@@ -0,0 +1,41 @@
+package thread;
+
+public class MainSynchronizedThreads {
+    private int count;
+    public static void main(String[] args) throws InterruptedException {
+        MainSynchronizedThreads mainSynchronizedThreads = new MainSynchronizedThreads();
+        mainSynchronizedThreads.doWork();
+    }
+
+    public synchronized void inc() {
+        count++;
+    }
+
+    public void doWork() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    inc();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000 ; i++) {
+                    inc();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(count);
+    }
+}
